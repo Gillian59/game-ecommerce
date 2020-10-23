86d3e677ffbf83db3736938d64f826b8340f4c5a
Philippe Adisson <philippe.adisson.pro@gmail.com>
2020-10-23 11:45:31 +0200

    Test end to end  + ajout de CSS sur les platforms

---
+++ src/test/e2e.test.ts
@@ -0,0 +1,145 @@
+import { openBrowser, closeBrowser, goto, text, click, waitFor, scrollDown, scrollUp, link } from "taiko";
+
+describe("Test d'accessibilité", () => {
+  jest.setTimeout(500000);
+
+  beforeAll(async () => {
+    await openBrowser({
+      args: [
+        "--window-size=1280,800",
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-first-run",
+        "--no-sandbox",
+        "--no-zygote",
+      ],
+      headless: false, //permet d'avoir un visuel graphique sur les tests//
+    });
+  });
+  afterAll(async () => {
+    await closeBrowser();
+  });
+  test("Chargement du Browser & Click sur Games", async () => {
+    //test open browser + click Games
+    expect.assertions(25);
+    const website = process.env.URL || "";
+    await goto(website);
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Next");
+    await click(link("Next"));
+    await click(link("Next"));
+    await click(link("Next"));
+    await click(link("Next"));
+    await click(link("Next"));
+    await click(link("Previous"));
+
+    /* TEST GAMES */
+
+    await scrollUp("Games");
+    expect(await text("Games").exists()).toBeTruthy();
+    await click(link("Games"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Minecraft");
+    await waitFor("Minecraft");
+    expect(await text("Minecraft").exists()).toBeTruthy();
+    await click(link("Minecraft"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Next");
+    await waitFor("Next");
+    expect(await text("Next").exists()).toBeTruthy();
+    await click(link("Next"));
+    await waitFor("Previous");
+    await click(link("Previous"));
+
+    await scrollUp("Nintendo Switch");
+    expect(await text("Nintendo Switch").exists()).toBeTruthy();
+    await waitFor("Nintendo Switch");
+    await click(link("Nintendo Switch"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Minecraft");
+    expect(await text("Minecraft").exists()).toBeTruthy();
+    await waitFor("Minecraft");
+    await click(link("Minecraft"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("PlayStation 4");
+    expect(await text("PlayStation 4").exists()).toBeTruthy();
+    await waitFor("PlayStation 4");
+    await click(link("PlayStation 4"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Minecraft");
+    expect(await text("Minecraft").exists()).toBeTruthy();
+    await waitFor("Minecraft");
+    await click(link("Minecraft"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Xbox 360");
+    expect(await text("Xbox 360").exists()).toBeTruthy();
+    await click(link("Xbox 360"));
+    await waitFor("Xbox 360");
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Minecraft");
+    expect(await text("Minecraft").exists()).toBeTruthy();
+    await waitFor("Minecraft");
+    await click(link("Minecraft"));
+
+    await scrollUp("Platforms");
+    expect(await text("Platforms").exists()).toBeTruthy();
+    await click(link("Platforms"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("Nintendo Switch");
+    expect(await text("Nintendo Switch").exists()).toBeTruthy();
+    await click("Nintendo Switch");
+    expect(await text("Xbox 360").exists()).toBeTruthy();
+    await click("Xbox 360");
+    expect(await text("PlayStation 4").exists()).toBeTruthy();
+    await click("PlayStation 4");
+
+    await scrollUp("VGD");
+    expect(await text("VGD").exists()).toBeTruthy();
+    await click(link("VGD"));
+
+    await scrollDown("©");
+    expect(await text("©").exists()).toBeTruthy();
+    await waitFor("©");
+
+    await scrollUp("API");
+    expect(await text("API").exists()).toBeTruthy();
+    await click("API");
+  });
+});
